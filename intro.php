c148ef4c47c50471479bc5d6a9ef048c61c0544e
asimdm <asimalidm.2020cs@technonjr.org>
2022-12-06 10:27:27 +0530

    new files

---
+++ intro.php
@@ -0,0 +1,25 @@
+<?php
+    $name="Abira-69";
+    echo "$name.<br>";
+
+    $a=0;
+    $b=10.5;
+    echo "value of a+b is ".$a+$b."<br>";
+    echo "value of a is ";
+    var_dump($a);
+    echo "<br>";
+    echo "value of b is ";
+    var_dump($b);
+    echo "<br>";
+    echo "value of a+b is ";
+    var_dump($a+$b);
+    echo "<br>";
+    echo "value of a*b is ";
+    var_dump($a*$b);
+    echo "<br>";
+
+    if($a)
+        echo "true";
+    else
+        echo "false";
+?>
